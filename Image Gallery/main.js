d721a43da42482ef73a15c0a1eac67d19cd1402b
Boye <samuel.boye95@yahoo.com>
2022-03-20 22:31:36 +0100

    Add Image Gallery folder and files

---
+++ Image Gallery/main.js
@@ -0,0 +1,15 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
