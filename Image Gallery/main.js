19f1db5b2757edc6ee5175386e0cc5bb34391cf9
Boye <samuel.boye95@yahoo.com>
2022-03-20 23:01:56 +0100

    add WiP

--- Image Gallery/main.js
+++ Image Gallery/main.js
@@ -5,11 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imgLinks = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imgLinks.length; i++) {
+    
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", imgLinks[i]);
+    thumbBar.appendChild(newImage);
+}
+thumbBar.addEventListener('click', (e) => {
+    displayedImage.sty.src = e
+});
 
 /* Wiring up the Darken/Lighten button */
