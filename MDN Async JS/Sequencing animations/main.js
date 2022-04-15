711b5692cc18666c2dbad83bf1b32739375ee00e
Boye <samuel.boye95@yahoo.com>
2022-04-15 14:57:32 +0100

    add solved and understood the basics of asynchronous js

--- MDN Async JS/Sequencing animations/main.js
+++ MDN Async JS/Sequencing animations/main.js
@@ -1,14 +1,31 @@
 const aliceTumbling = [
-  { transform: 'rotate(0) scale(1)' },
-  { transform: 'rotate(360deg) scale(0)' }
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
 ];
 
 const aliceTiming = {
   duration: 2000,
   iterations: 1,
-  fill: 'forwards'
-}
+  fill: "forwards",
+};
 
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+// alice1.animate(aliceTumbling, aliceTiming).finished.then( () => {
+//   alice2.animate(aliceTumbling, aliceTiming).finished.then( () => {
+//     alice3.animate(aliceTumbling, aliceTiming)
+//   })
+// });
+
+// alice1
+//   .animate(aliceTumbling, aliceTiming)
+//   .finished.then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+//   .then(() => alice3.animate(aliceTumbling, aliceTiming).finished);
+
+(async function aliceAnimation() {
+  await alice1.animate(aliceTumbling, aliceTiming).finished;
+  await alice2.animate(aliceTumbling, aliceTiming).finished;
+  await alice3.animate(aliceTumbling, aliceTiming).finished;
+}());
